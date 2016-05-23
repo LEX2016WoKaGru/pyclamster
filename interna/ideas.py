63d46dc45968e6ac35428b8c835ad8e200d7aa3a
Yann Büchau <yann.buechau@web.de>
2016-05-23 12:53:52 +0200

    updated ideas.py with new ideas

--- interna/ideas.py
+++ interna/ideas.py
@@ -21,8 +21,11 @@ Created for pyClamster
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 # System modules
+import os,sys,time
 
 # External modules
+import scipy as sp
+import numpy as np
 
 # Internal modules
 
@@ -31,27 +34,51 @@ __version__ = "0.0.0.1"
 
 
 class Camera(object):
+    """
+    class that holds a series of images
+    and camera properties
+    """
     def __init__(self):
-        self.image_series = {timestamp: Image}
-        self.displacement = {Azi, Ele}
+        self.image_series     = {timestamp: Image}
+        self.Azi_displacement = {timestamp: Azi}
+        self.Ele_displacement = {timestamp: Ele}
 
-    def getElevation(self):
-        pass
+    def getImage(self,timestamp):
+        """
+        return an instance of class Image() for the timestamp timestamp
+        """
+        image = Image(timestamp) # TODO
+        return image
 
 
 class Image(object):
-    def __init__(self, data=None):
+    def __init__(self, path=None,data=None, elevation=None, azimuth=None):
+        """
+        args:
+            data(numpy array): RBG values for each pixel  - shape(width,height,3)
+            elevation(numpy array): elevation for each pixel - shape(width,height)
+            azimuth(numpy array): azimuth for each pixel - shape(width,height)
+        """
         self.data = data
-        self.degrees = {"X": 180, "Y": 180}
+        self.elevation = elevation
+        self.azimuth = azimuth
+
+        # load image from path if specified
+        if isinstance(path,str) and os.path.isfile(path):
+            self.loadImage(path)
 
     def loadImage(self, path):
-        pass
+        """
+        read image from path
+        """
+        img = sp.ndimage.imread(path, mode="RGB")
+        self.data = img
 
     def saveImage(self, path):
-        pass
-
-    def applyMask(self, mask):
-        pass
+        """
+        save image to path
+        """
+        img = sp.misc.imsave(path, self.data)
 
     def _calcCenter(self):
         pass
@@ -73,22 +100,64 @@ class Image(object):
     def std(self):
         pass
 
-    def getElevation(self):
+    def getElevation(self,timestamp):
+        """
+        return elevation for each pixel as a numpy array
+        
+        """
         pass
 
-    def getAzimuth(self, dis_in_deg=0):
+    def getAzimuth(self):
+        """
+        return elevation for each pixel as a numpy array
+        
+        """
+        pass
+
+    def applyMask(self, mask):
+        """
+        args:
+            mask (numpy mask): mask to be applied
+        returns:
+            maskedimage( Image ): new instance of Image class with applied mask
+        """
         pass
 
 
-class MaskedImage(object):
-    def __init__(self, raw):
-        assert isinstance(raw, Image)
-        self.raw = raw
-        self.masks = masks
+class MaskStore(object):
+    def __init__(self):
+        self.masks = {}
+    
+    def addMask(self, mask, label=None):
+        """
+        args:
+            mask (numpy array): mask (0=pixel unused, 1=pixel used) - shape(width,height)
+            label (optional[int or str]): label of the mask. Default automatic numbering
+        """
+        if label is None:
+            pass
+
+        self.masks[label] = mask
 
 
-    def addMask(self, mask):
-        self.masks.append(mask)
+    def removeMask(self, label):
+        """
+        remove mask with label label from mask list
+        args:
+            label (int or str): label of the mask. 
+        """
+
+        del self.masks[label]
+
+
+    def getMask(self, labels=None):
+        """
+        return merged numpy array out of selected masks
+        args:
+            label (list of int or str): list of mask labels to be merged. Defaults to all masks.
+        """
+        pass
+        
 
 class TrashDetector(object):
     def detect(self, image):
