1e17c07fe9e70d478698d2ac0976cbdcbc007f5c
maestro <t.finn@meteowindow.com>
2016-06-02 18:41:05 +0200

    Added a preprocess file for the clustering algorithm

---
+++ pyclamster/clustering/preprocess.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 02.06.16
+
+Created for pyclamster
+
+    Copyright (C) {2016}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# System modules
+
+# External modules
+
+# Internal modules
+
+
+__version__ = "0.1"
+
+"""
+This module is for pre-processing transformations for the cloud detection
+algorithms.
+"""
+
+class PreProcess(object):
+    pass
