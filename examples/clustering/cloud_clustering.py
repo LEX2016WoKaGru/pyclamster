7e4ceea5c20aee40a617c2e1317061fac8ecd456
maestro <t.finn@meteowindow.com>
2016-08-13 17:54:26 +0200

    Little improvement in the cloud clustering example.

--- examples/clustering/cloud_clustering.py
+++ examples/clustering/cloud_clustering.py
@@ -57,7 +57,7 @@ warnings.filterwarnings('ignore')
 
 __version__ = ""
 
-base_folder = "/home/tfinn/Projects/pyclamster/"
+base_folder = "../../"
 image_directory = os.path.join(base_folder, "examples", "images", "wettermast")
 trained_models = os.path.join(base_folder, "trained_models")
 
@@ -66,48 +66,24 @@ center = int(1920/2)
 good_angle_dpi = int(np.round(1920 / 180 * good_angle))
 denoising_ratio = 10
 all_images = glob.glob(os.path.join(image_directory, "Image_Wkm_Aktuell_*.jpg"))
-#print(all_images)
-#all_images = [os.path.join(image_directory, "Image_Wkm_Aktuell_2.jpg"),]
 
 
-
-predictor = pickle.load(open(os.path.join(trained_models, "elm.pk"), "rb"))
+predictor = pickle.load(open(os.path.join(trained_models, "kmeans.pk"), "rb"))
 
 for image_path in all_images:
-    img = scipy.misc.imread(image_path, flatten=False, mode="RGB") / 256
-    img = LCN(size=(50, 50, 3), scale=False).fit_transform(img)
-    img = img[480:1480, 480:1480]
-    raw = data = scipy.misc.imresize(img, (249, 249), interp="bicubic").astype(
-        'float64')
-    scipy.misc.imsave("original.jpg", raw)
-    data = data.reshape((-1, 3))
-    data = data - data.mean(axis=0)
-    std = data.std(axis=0)
-    std[std < 0.001] = 0.001
-    data = data / std
-    # image = Image(image_path)
-    # image.data = image.data/256
-    # image.data = LCN(size=(50, 50, 3), scale=False).fit_transform(image.data)
-    # im = img[480:1480, 480:1480]
-    #
-    # image.data = scipy.misc.imresize(image.data, 0.25, interp='bicubic')
-    # cutted_image = image.cut([120, 120, 360, 360])
-    # # cutted_image = image.cut([center - good_angle_dpi, center - good_angle_dpi,
-    # #                           center + good_angle_dpi, center + good_angle_dpi])
-    # cutted_image.save("original.png")
-    # image.data = LCN(size=(13,13,3), scale=False).fit_transform(image.data)
-    # image = image.cut([120, 120, 360, 360])
-    # w, h, _ = original_shape = image.data.shape
-    # raw_image = rbDetection(image.data).reshape((w*h, -1))
-    #raw_image = image.data.reshape((w*h, -1))
-    label,_,_ = predictor.predict(data)
-    label = Labels(label)
-    label.reshape((249, 249), replace=True)
+    image = Image(image_path)
+    image.data = LCN(size=(50,50,3), scale=False).fit_transform(image.data/256)
+    image.data = image.data[center - good_angle_dpi:center + good_angle_dpi,
+                            center - good_angle_dpi:center + good_angle_dpi]
+    raw_image = rbDetection(image.data)
+    w, h = original_shape = tuple(raw_image[:, :].shape)
+    raw_image = np.reshape(raw_image, (w * h, 1))
+    label = predictor.predict(raw_image)
+    label.reshape((960, 960), replace=True)
     scipy.misc.imsave("cloud.png", label.labels)
     masks = label.getMaskStore()
-    masks.denoise([3], 50)
-    cloud_labels, _ = masks.labelMask([3,])
-    #scipy.misc.imsave("labels.png", cloud_labels.labels)
+    masks.denoise([0], 960)
+    cloud_labels, _ = masks.labelMask([0,])
+    scipy.misc.imsave("labels.png", cloud_labels.labels)
     scipy.misc.imshow(cloud_labels.labels)
     cloud_store = cloud_labels.getMaskStore()
-    #clouds = [cloud_store.getCloud(raw, [k,]) for k in cloud_store.masks.keys()]
