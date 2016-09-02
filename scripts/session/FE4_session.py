c31bc568cf0605627a4bc4823bf2b71eddbba968
Yann Büchau <yann.buechau@web.de>
2016-09-02 16:25:40 +0200

    added pickle files and sessions

--- scripts/session/FE4_session.py
+++ scripts/session/FE4_session.py
@@ -19,20 +19,20 @@ except: # if not
         longitude  = 54.49587,
         latitude   = 11.237683,
         imgshape   = (1920,1920),
-        smallshape = (300,300),
-        rectshape  = (200,200),
+        smallshape = (500,500),
+        rectshape  = (300,300),
         calibration = calib
         )
     # add images to session
-    session.add_images("/home/yann/Studium/LEX/LEX/cam/cam4/20160901/FE4_Image_20160901_09*.jpg")
+    session.add_images("/home/yann/Studium/LEX/LEX/cam/cam4/20160901/*.jpg")
     
     # create distortion map
-    session.createDistortionMap()
+    session.createDistortionMap(max_angle=pyclamster.deg2rad(75))
 
     # save thie session
     session.save(sessionfile)
 
 # loop over all images
 for image in session.iterate_over_rectified_images():
-    image.show()
-    sys.stdin.read(1)
+    filename = image._get_time_from_filename("FE4_Image_%Y%m%d_%H%M%S_UTCp1.jpg")
+    image.image.save("plots/images/fe4/{}-rect.jpg".format(filename))
