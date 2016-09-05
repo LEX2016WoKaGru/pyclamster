0b12ea4da0bc6bf81dc92d1669762f43142ed730
Yann Büchau <yann.buechau@web.de>
2016-09-05 10:56:35 +0200

    improved doppelanschnitt_Coordinates3d

--- pyclamster/positioning.py
+++ pyclamster/positioning.py
@@ -87,21 +87,21 @@ def doppelanschnitt_Coordinates3d(aziele1,aziele2,pos1,pos2):
             Doppelanschnitt.
     """
     # turn to north
-    if aziele1.elevation_type=="zenith":
-        aziele1.elevation = np.pi/2 - aziele1.elevation
+    aziele1.radius = 1
     aziele1.change_parameters(
-        azimuth_offset = 3/2 * np.pi, azimuth_clockwise = True,
+        azimuth_offset = 3/2 * np.pi,
+        azimuth_clockwise = True,
         elevation_type = "ground",
-        keep = {'azimuth','elevation'}
+        keep = {'x','y','z'}
         )
-
     logger.debug("aziele1: \n{}".format(aziele1))
-    if aziele2.elevation_type=="zenith":
-        aziele2.elevation = np.pi/2 - aziele2.elevation
+
+    aziele2.radius = 1
     aziele2.change_parameters(
-        azimuth_offset = 3/2 * np.pi, azimuth_clockwise = True,
+        azimuth_offset = 3/2 * np.pi, 
+        azimuth_clockwise = True,
         elevation_type = "ground",
-        keep = {'azimuth','elevation'}
+        keep = {'x','y','z'}
         )
     logger.debug("aziele2: \n{}".format(aziele2))
 
---
+++ scripts/doppel/doppel_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import pyclamster
+import numpy as np
+import matplotlib.pyplot as plt
+
+theo1 = pyclamster.Coordinates3d(
+    azimuth =   [0],
+    elevation = [pyclamster.deg2rad(45)],
+    azimuth_clockwise = False,
+    azimuth_offset = 0,
+    elevation_type = "zenith"
+    )
+
+pos1 = pyclamster.Coordinates3d(
+    x = 0, y = 0, z = 0
+    )
+
+
+theo2 = pyclamster.Coordinates3d(
+    azimuth =   [np.pi],
+    elevation = [pyclamster.deg2rad(45)],
+    azimuth_clockwise = True,
+    azimuth_offset = 0,
+    elevation_type = "ground"
+    )
+
+pos2 = pyclamster.Coordinates3d(
+    x = 1, y = 1, z = 0
+    )
+    
+doppel = pyclamster.positioning.doppelanschnitt_Coordinates3d(
+    aziele1 = theo1,
+    aziele2 = theo2,
+    pos1    = pos1,
+    pos2    = pos2
+    )
+
+ax = doppel.plot3d()
+ax.set_xlim(0,1)
+ax.set_ylim(-1,1)
+ax.set_zlim(0,1)
+ax.scatter3D(pos1.x,pos1.y,pos1.z,color='r')
+ax.scatter3D(pos2.x,pos2.y,pos2.z,color='g')
+plt.show()
